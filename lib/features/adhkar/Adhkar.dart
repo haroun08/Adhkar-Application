66730cd33c81c9cbc72bc58538f669a29226403d
haroun08 <129984358+haroun08@users.noreply.github.com>
2024-07-11 20:34:10 +0100

    managing state

--- lib/features/adhkar/Adhkar.dart
+++ lib/features/adhkar/Adhkar.dart
@@ -1,8 +1,8 @@
-import 'dart:async';
-import 'package:audioplayers/audioplayers.dart';
-import 'package:flutter/material.dart';
 import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:audioplayers/audioplayers.dart';
+
 import '../../models/AdhkarItem .dart';
 
 class Adhkar extends StatefulWidget {
@@ -13,7 +13,7 @@ class Adhkar extends StatefulWidget {
 }
 
 class _AdhkarState extends State<Adhkar> {
-  List<AdhkarItem>? _adhkarItems; // Declare as nullable initially
+  List<AdhkarItem>? _adhkarItems;
   final AudioPlayer _audioPlayer = AudioPlayer();
 
   @override
@@ -23,7 +23,8 @@ class _AdhkarState extends State<Adhkar> {
   }
 
   Future<void> _loadAdhkarData() async {
-    final String jsonString = await rootBundle.loadString('assets/adhkar.json');
+    final String jsonString =
+    await rootBundle.loadString('assets/adhkar.json');
     final jsonData = jsonDecode(jsonString);
     setState(() {
       _adhkarItems = List<AdhkarItem>.from(
@@ -42,94 +43,162 @@ class _AdhkarState extends State<Adhkar> {
     }
   }
 
+  void _stopAudio() async {
+    await _audioPlayer.stop();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'Adhkar',
+        centerTitle: true,
+        title: Text(
+          'أذكار',
           style: TextStyle(
             fontWeight: FontWeight.bold,
             fontSize: 25,
           ),
+          textAlign: TextAlign.center,
         ),
       ),
       body: _adhkarItems == null
           ? Center(
         child: CircularProgressIndicator(),
       )
-          : GridView.builder(
+          : ListView.builder(
         padding: const EdgeInsets.all(8),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 1,
-          crossAxisSpacing: 8,
-          mainAxisSpacing: 8,
-        ),
         itemCount: _adhkarItems!.length,
         itemBuilder: (context, index) {
           final adhkarItem = _adhkarItems![index];
-          return Card(
-            elevation: 4,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    adhkarItem.category,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => AdhkarDetails(adhkarItem: adhkarItem),
+                ),
+              );
+            },
+            child: Card(
+              elevation: 4,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      adhkarItem.category,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                      textAlign: TextAlign.right,
+                      textDirection: TextDirection.rtl,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
                     ),
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  SizedBox(height: 8),
-                  Expanded(
-                    child: Text(
+                    SizedBox(height: 8),
+                    Text(
                       adhkarItem.array[0]['text'],
+                      textAlign: TextAlign.right,
+                      textDirection: TextDirection.rtl,
                       maxLines: 3,
                       overflow: TextOverflow.ellipsis,
                     ),
-                  ),
-                  SizedBox(height: 8),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      IconButton(
-                        icon: Icon(Icons.favorite,
-                            color: adhkarItem.isFavorite
-                                ? Colors.red
-                                : Colors.grey),
-                        onPressed: () {
-                          setState(() {
-                            adhkarItem.isFavorite =
-                            !adhkarItem.isFavorite;
-                          });
-                        },
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.play_arrow),
-                        onPressed: () {
-                          _playAudio(adhkarItem.audio);
-                        },
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          setState(() {
-                            adhkarItem.count++;
-                          });
-                        },
-                        child: Text(adhkarItem.count.toString()),
-                      ),
-                    ],
-                  ),
-                ],
+                    SizedBox(height: 8),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.favorite,
+                              color: adhkarItem.isFavorite
+                                  ? Colors.red
+                                  : Colors.grey),
+                          onPressed: () {
+                            setState(() {
+                              adhkarItem.isFavorite =
+                              !adhkarItem.isFavorite;
+                            });
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.play_arrow),
+                          onPressed: () {
+                            _playAudio(adhkarItem.audio);
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.stop),
+                          onPressed: () {
+                            _stopAudio();
+                          },
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            setState(() {
+                              adhkarItem.count++;
+                            });
+                          },
+                          child: Text(adhkarItem.count.toString()),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           );
         },
+      ),
+    );
+  }
+}
+
+
+class AdhkarDetails extends StatelessWidget {
+  final AdhkarItem adhkarItem;
+
+  const AdhkarDetails({required this.adhkarItem});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          adhkarItem.category,
+          textAlign: TextAlign.center,
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              adhkarItem.array[0]['text'],
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.right,
+            ),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.play_arrow),
+                  onPressed: () {
+                    // Play audio logic here
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.stop),
+                  onPressed: () {
+                    // Stop audio logic here
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/features/categories/cubit/categories_cubit.dart
@@ -0,0 +1,9 @@
+import "package:bloc/bloc.dart";
+import 'package:meta/meta.dart';
+
+part 'categories_state.dart';
+
+class CategoriesCubit extends Cubit<CategoriesState>{
+  CategoriesCubit() : super(CategoriesInitial());
+}
+
